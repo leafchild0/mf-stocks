f8b5ca146fb56f60926e6b3e77e4aa3cdd0cae81
Victor Malyshev <vmalyshev0@gmail.com>
2020-03-10 13:40:15 +0200

    Added consul, disabling security

--- auth-server/src/main/java/com/poc/authserver/security/SecurityConfig.java
+++ auth-server/src/main/java/com/poc/authserver/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.authorizeRequests()
-			.antMatchers("/",
+			.antMatchers("/*",
 				"/favicon.ico",
 				"/**/*.png",
 				"/**/*.gif",
@@ -90,9 +90,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 			.antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
 			.permitAll()
 			.antMatchers(HttpMethod.GET, "/api/users/**")
-			.permitAll()
-			.anyRequest()
-			.authenticated();
+			.permitAll();
+			//.anyRequest()
+			//.authenticated();
 
 		// Add our custom JWT security filter
 		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
