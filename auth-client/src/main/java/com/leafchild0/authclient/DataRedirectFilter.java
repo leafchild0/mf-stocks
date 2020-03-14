e17632858960eceb8c44f12805672b46e13099d2
Victor Malyshev <vmalyshev0@gmail.com>
2020-03-14 17:19:31 +0200

    Added Zuul filters for all types of operations

--- auth-client/src/main/java/com/leafchild0/authclient/DataRedirectFilter.java
+++ auth-client/src/main/java/com/leafchild0/authclient/DataRedirectFilter.java
@@ -5,11 +5,13 @@ import com.netflix.zuul.context.RequestContext;
 import org.springframework.stereotype.Component;
 
 /**
+ * Has to check if request goes to /data and redirect request to appropriate service
+ *
  * @author victor
  * @date 09.03.2020
  */
 @Component
-public class TokenCheckFilter extends ZuulFilter {
+public class DataRedirectFilter extends ZuulFilter {
 
 	@Override
 	public String filterType() {
@@ -27,13 +29,13 @@ public class TokenCheckFilter extends ZuulFilter {
 	public boolean shouldFilter() {
 
 		RequestContext ctx = RequestContext.getCurrentContext();
-		return ctx.getRequest().getParameter("Authorization") != null;
+		return ctx.getRequest().getRequestURI().endsWith("/data");
 	}
 
 	@Override
 	public Object run() {
 
-		// Make a call to auth service and check token is valid
+		// Perform actual redirection
 		return null;
 	}
 }
