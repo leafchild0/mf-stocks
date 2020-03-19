958676d7fabf66a59466ecbb48461dba7e748af5
Victor Malyshev <vmalyshev0@gmail.com>
2020-03-20 00:33:57 +0200

    Minor cleanupAdding API to bypass security

--- auth-client/src/main/java/com/leafchild0/authclient/SecurityConfig.java
+++ auth-client/src/main/java/com/leafchild0/authclient/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -46,13 +45,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	protected void configure(HttpSecurity http) throws Exception
 	{
 		http
-			.cors()
-			.and()
-			.csrf()
-			.disable()
-			.headers().frameOptions().sameOrigin()
-			.and()
-			.authorizeRequests()
+				.cors()
+				.and()
+				.csrf()
+				.disable()
+				.headers().frameOptions().sameOrigin()
+				.and()
+				.authorizeRequests()
+				.antMatchers("/api/login", "/api/data")
+				.permitAll()
 			.antMatchers("/*",
 				"/favicon.ico",
 				"/**/*.png",
