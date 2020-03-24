6d8dc97fcbecb2de33e99de5a7ad63ad025ea61a
Victor Malyshev <vmalyshev0@gmail.com>
2020-03-24 16:37:07 +0200

    Gateway service cleanup

--- gateway-service/src/main/java/com.poc.gateway/health/ServiceDiscoveryController.java
+++ gateway-service/src/main/java/com.poc.gateway/health/ServiceDiscoveryController.java
@@ -6,8 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * An example of custom health check for Consul
+ */
 @RestController
 public class ServiceDiscoveryController {
+
 	@GetMapping("/health-check")
 	public ResponseEntity<String> myCustomCheck() {
 		String message = "Testing my healh check function";
--- gateway-service/src/main/java/com.poc.gateway/security/JwtTokenAuthenticationFilter.java
+++ gateway-service/src/main/java/com.poc.gateway/security/JwtTokenAuthenticationFilter.java
@@ -1,22 +1,23 @@
 package com.poc.gateway.security;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import io.jsonwebtoken.Claims;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * Filter that initiates token validation
+ */
 public class JwtTokenAuthenticationFilter extends OncePerRequestFilter
 {
 	private JwtTokenProvider tokenProvider;
@@ -30,15 +31,15 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter
 	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse rsp, FilterChain filterChain)
 		throws ServletException, IOException {
 		String token = getJwtFromRequest(req);
-		if (StringUtils.hasText(token) && tokenProvider.validateToken(token))
-		{
+		if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
 			try {
+
 				Claims claims = tokenProvider.getClaims(token);
 				Long userId = tokenProvider.getUserIdFromJWT(claims);
 
-//				String username = claims.getSubject();
 				@SuppressWarnings("unchecked")
 				List<String> authorities = claims.get("authorities", List.class);
+
 				if (userId != null) {
 					UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userId, null,
 						authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
@@ -51,11 +52,10 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter
 		filterChain.doFilter(req, rsp);
 	}
 
-	private String getJwtFromRequest(HttpServletRequest request)
-	{
+	private String getJwtFromRequest(HttpServletRequest request) {
+
 		String bearerToken = request.getHeader("Authorization");
-		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer "))
-		{
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
 			return bearerToken.substring(7);
 		}
 		return null;
