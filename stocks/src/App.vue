3dba7df7cabde6dba5ba12c4396d96d96aa1a0b9
Victor Malyshev <vmalyshev0@gmail.com>
2020-08-07 15:07:08 +0300

    #7: Basics of stocks is ready

--- stocks/src/App.vue
+++ stocks/src/App.vue
@@ -1,20 +1,47 @@
 <template>
-  <v-app>
-    <v-main>
-    </v-main>
-  </v-app>
+	<v-app>
+		<v-main>
+			<v-text-field
+				v-model="search"
+				class="mx-4"
+				flat
+				hide-details
+				label="Search">
+			</v-text-field>
+
+			<stock :stocks="filtered"></stock>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
 
+import Stock from './components/Stock';
+import auth from './auth/authApi';
+
 export default {
-  name: 'App',
+	name: 'App',
 
-  components: {
-  },
-
-  data: () => ({
-    //
-  }),
+	components: {
+		Stock
+	},
+	computed: {
+		filtered() {
+			if (this.search.length < 3) return this.stocks;
+			else return this.stocks.filter(s => s.name.toLowerCase().includes(this.search.toLowerCase()));
+		}
+	},
+	data: () => ({
+		stocks: [],
+		search: ''
+	}),
+	async created() {
+		try {
+			const stocksResponse = await auth.get('/stocks');
+			this.stocks = stocksResponse.data;
+		} catch (e) {
+			console.error(e);
+		}
+	}
 };
 </script>
---
+++ stocks/src/components/Stock.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="stocks">
+		<v-row align="center" justify="center">
+			<v-card
+				:key="s.id"
+				v-for="s in stocks"
+				class="stock d-block d-inline-block"
+				max-width="500"
+				width="300"
+				outlined>
+				<v-list-item>
+					<v-list-item-avatar color="grey"></v-list-item-avatar>
+					<v-list-item-content>
+						<v-list-item-title class="headline">{{ s.name }}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+
+				<v-card-text>
+					<div class="stock-price text-h6">Price: {{ s.price }}</div>
+					<div class="stock-amount subtitle-1">Price: {{ s.amount }}</div>
+				</v-card-text>
+
+				<v-card-actions>
+					<v-btn>Buy</v-btn>
+					<v-spacer></v-spacer>
+					<v-text-field label="Amount" dense hide-details></v-text-field>
+				</v-card-actions>
+			</v-card>
+		</v-row>
+	</div>
+
+</template>
+
+<script>
+export default {
+	name: 'stock',
+	props: {
+		stocks: Array
+	}
+};
+</script>
+
+<style scoped lang='scss'>
+
+	.stocks {
+		width: 100%;
+		height: 100%;
+	}
+
+	.stock {
+		margin: 20px;
+	}
+
+</style>
