6b5841321be4b749497124fd8e45aea053c0528c
Rybak Roman <r.romanv1@gmail.com>
2020-10-03 14:42:36 +0300

    #8: Add post api for history

--- history/server/src/test/kotlin/com/kotlinspringvue/backend/HistoryControllerTest.kt
+++ history/server/src/test/kotlin/com/kotlinspringvue/backend/HistoryControllerTest.kt
@@ -1,15 +1,17 @@
 package com.kotlinspringvue.backend
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 
 /**
  * Created by romanrybak on 07.09.2020.
@@ -23,9 +25,32 @@ class HistoryControllerTests {
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @Autowired
+    lateinit var mapper: ObjectMapper
+
     @Test
     fun `Test get History in the happy path scenario`() {
         mockMvc.get("/history")
                 .andExpect { status { isOk } }
+    }
+
+    @Test
+    fun `Test add History in the happy path scenario`() {
+        val testHistory = History("TestStockName", "", "user", "21", "BUY")
+        val expectationTestHistory = History("TestStockName", "", "user", "21", "BUY")
+        expectationTestHistory.id = 1
+        doTest(testHistory, expectationTestHistory)
+    }
+
+    private fun doTest(input: History, expectation: History) {
+        mockMvc.post("/history") {
+            contentType = MediaType.APPLICATION_JSON
+            content = mapper.writeValueAsString(input)
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            content { json(mapper.writeValueAsString(expectation)) }
+        }
     }
 }
