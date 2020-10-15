f9ed6caa469335dfd6b672c88ec6945baea1ba9a
Victor Malyshev <vmalyshev0@gmail.com>
2020-10-15 12:35:37 +0300

    #20 Code style

--- stocks/server.js
+++ stocks/server.js
@@ -22,16 +22,14 @@ app.get('*', (req, res) => {
 
 // Connect to DB
 db.mongoose.connect(db.url, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
-	.then(() => {
-		console.log('Connected to the database!');
-	})
-	.catch(err => {
-		console.log('Cannot connect to the database!', err);
-		process.exit();
-	});
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}).then(() => {
+	console.log('Connected to the database!');
+}).catch(err => {
+	console.log('Cannot connect to the database!', err);
+	process.exit();
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
