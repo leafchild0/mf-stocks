f0e49fd6b3d0aa91273585538c1061f743f6f267
Victor Malyshev <vmalyshev0@gmail.com>
2020-10-15 12:34:59 +0300

    #20 Adding everything in server

--- stocks/server.js
+++ stocks/server.js
@@ -4,23 +4,36 @@
  */
 
 const express = require('express');
-const data = require('./db.json');
+const bodyParser = require('body-parser');
 const app = express();
+const db = require('./src/models');
+app.use(bodyParser.json());
 
+// Routes
+require('./src/stocks.routes')(app);
 app.use(express.static('dist'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-app.get('/stocks', (req, res) =>
-{
-	res.json(data.stocks);
-});
-
-app.get('*', (req, res) =>
-{
+app.get('*', (req, res) => {
 	res.redirect('/');
 });
 
+// Connect to DB
+db.mongoose.connect(db.url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		console.log('Connected to the database!');
+	})
+	.catch(err => {
+		console.log('Cannot connect to the database!', err);
+		process.exit();
+	});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-{
+app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
 });
