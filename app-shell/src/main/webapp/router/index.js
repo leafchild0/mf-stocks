768a7c565920444904220a68ac1164406666dc6e
Victor Malyshev <ts-victor.malyshev@rakuten.com>
2020-12-19 20:04:33 +0200

    Routes

--- app-shell/src/main/webapp/router/index.js
+++ app-shell/src/main/webapp/router/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Stocks from '../views/Stocks.vue';
-import PurchaseHistory from '../views/History.vue';
 import Login from '../views/Login.vue';
 import SignUp from '../views/SignUp.vue';
-import Cart from '../views/Cart.vue';
-import store from '../store/index';
+import Content from '@/views/Content';
+import UserAccount from '@/views/UserAccount';
+import Cart from '@/views/Cart';
 
 Vue.use(VueRouter);
 
@@ -21,17 +20,9 @@ const routes = [
 		component: Login
 	},
 	{
-		path: '/stocks',
-		name: 'stocks',
-		component: Stocks,
-		meta: {
-			requiresAuth: true
-		}
-	},
-	{
-		path: '/history',
-		name: 'history',
-		component: PurchaseHistory,
+		path: '/',
+		name: 'content',
+		component: Content,
 		meta: {
 			requiresAuth: true
 		}
@@ -40,6 +31,14 @@ const routes = [
 		path: '/cart',
 		name: 'cart',
 		component: Cart,
+		meta: {
+			requiresAuth: true
+		}
+	},
+	{
+		path: '/',
+		name: 'user',
+		component: UserAccount,
 		meta: {
 			requiresAuth: true
 		}
@@ -57,19 +56,19 @@ const router = new VueRouter({
 	routes
 });
 
-router.beforeEach((to, from, next) => {
-
-	// We can just check if token is there
-	const currentUser = store.state.isUserLogged;
-	const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-
-	if (requiresAuth && !currentUser) {
-		next('login');
-	} else if (!requiresAuth && currentUser) {
-		next('stocks');
-	} else {
-		next();
-	}
-});
+// router.beforeEach((to, from, next) => {
+//
+// 	// We can just check if token is there
+// 	const currentUser = store.state.isUserLogged;
+// 	const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+//
+// 	if (requiresAuth && !currentUser) {
+// 		next('login');
+// 	} else if (!requiresAuth && currentUser) {
+// 		next('content');
+// 	} else {
+// 		next();
+// 	}
+// });
 
 export default router;
